5af5851976d401bfa50272c8f8fa1856544a31b6
lyber_spqr <artuom0405@gmail.com>
2025-08-21 18:08:36 +0300

    Removed unnecessary comments

--- src/main/java/com/innowise/OrderService/service/OrderService.java
+++ src/main/java/com/innowise/OrderService/service/OrderService.java
@@ -13,10 +13,8 @@ import com.innowise.OrderService.mapper.OrderMapper;
 import com.innowise.OrderService.repository.ItemRepository;
 import com.innowise.OrderService.repository.OrderRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.data.jpa.repository.EntityGraph;
+
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.request.RequestContextHolder;
