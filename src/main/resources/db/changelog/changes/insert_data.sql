740a516f842627ef995902f09115b9d3a8bd5034
lyber_spqr <artuom0405@gmail.com>
2025-08-25 15:47:34 +0300

    deleted test sql insertions

--- src/main/resources/db/changelog/changes/insert_data.sql
+++ src/main/resources/db/changelog/changes/insert_data.sql
@@ -4,19 +4,3 @@ INSERT INTO items (name, price) VALUES
                                     ('laptop', 999.99),
                                     ('smartphone', 699.99),
                                     ('headphones', 149.99);
-
--- --changeset admin:5
--- INSERT INTO orders (user_id, status, creation_date) VALUES
---                                                         (1, 'CREATED', '2024-10-01'),
---                                                         (2, 'PROCESSING', '2023-10-02'),
---                                                         (3, 'COMPLETED', '2022-10-03');
---
--- --changeset admin:6
--- INSERT INTO order_items (quantity, order_id, item_id) VALUES
---
---
---
---
---
---
---
