c2155ab67e26a2cbdc30e1ca25716be750385173
lyber_spqr <artuom0405@gmail.com>
2025-10-06 18:45:52 +0300

    fix userServiceUrl

--- src/main/java/com/innowise/OrderService/configuration/WebClientConfiguration.java
+++ src/main/java/com/innowise/OrderService/configuration/WebClientConfiguration.java
@@ -10,7 +10,7 @@ public class WebClientConfiguration {
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
 
-       return builder.baseUrl("http://app1:8081").build(); // 8081 if docker
+       return builder.baseUrl("http://userservice:8081").build(); // 8081 if docker
 
     }
 }
